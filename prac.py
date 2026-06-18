b4922bc8f16208e3c9560f5631718da72b8f9b9a
Ayush <ayushtamboli708@gmail.com>
2026-06-19 01:40:43 +0530

    add error correction

---
+++ prac.py
@@ -0,0 +1,7 @@
+a= 8
+b=7
+print(f'{a} + {b} = {a+b}')
+radie = 10
+area = 3.14*radie**2
+print(f'are of circle with radie{radie} is {area}')
+print("name\t age\t city\t \n","ysuh\t 250\t finland\t")
--- tracker.py
+++ tracker.py
@@ -8,14 +8,24 @@ class ExpenceTracker:
         while b:
             key = input("what you spend on: ")
             value = input("how much you spend: ")
-            ExpenceTracker.expence_list.append({"what you spend on":key, "how much you spend":value})
-            c=input("do you want to add more expense? (y/n)")
-            if c == "y":
-                b=True
+            if value.isdigit():
+                value = int(value)
+                ExpenceTracker.expence_list.append({"what you spend on":key, "how much you spend":value})
+                c=input("do you want to add more expense? (y/n)")    
+                if c == "y":
+                    b=True
+                else:
+                    b=False
             else:
-                b=False
+                print("Invalid input. Please enter a valid number.")
 
     def view_expense(self):
+        try:
+            if len(ExpenceTracker.expence_list) == 0:
+                raise ValueError("No expense found. Please add some expense first.")
+        except ValueError:
+            print("No expense found. Please add some expense first.")
+            return
         print("Expense List: \n")
         for item in ExpenceTracker.expence_list:
             print( f"what you spend on: {item['what you spend on']} : how much you spend: {item['how much you spend']}\n")
