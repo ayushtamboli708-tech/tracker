d7cdbd1ce37ba5d0b4764ca2fa7df8a98fa8c149
Ayush <ayushtamboli708@gmail.com>
2026-06-20 22:21:44 +0530

    practice

--- prac.py
+++ prac.py
@@ -1,12 +1,17 @@
 
-tp=()
-tp=("ayu","mom","di")
-tp1=("bull","bill")
-sib=tp+tp1
-tp3=("mome","duda")
-sib=sib+tp3
-sol=sib[:5]
-print(len(sib))
-print((sol))
-tp2=list(tp)
-print(type(tp2))
+# sets
+it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
+A = {19, 22, 24, 20, 25, 26}
+B = {19, 22, 20, 25, 26, 24, 28, 27}
+age = [22, 19, 24, 25, 26, 24, 25, 24]
+print(len(it_companies))
+it_companies.add("twitter")
+print('twitter' in it_companies)
+com={"com1","compe2","cpm3"}
+it_companies.update(com)
+it_companies.pop()
+print(A.union(B))
+print(B.union(A))
+print(A.isdisjoint(B))
+print(A.symmetric_difference(B))
+del it_companies
