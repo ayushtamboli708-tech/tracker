9f7cbea460e28e0b301b19d1a4410f0b072f8650
Ayush <ayushtamboli708@gmail.com>
2026-06-21 22:52:32 +0530

    pract

--- prac.py
+++ prac.py
@@ -1,26 +1,10 @@
 
-# sets
-it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
-A = {19, 22, 24, 20, 25, 26}
-B = {19, 22, 20, 25, 26, 24, 28, 27}
-age = [22, 19, 24, 25, 26, 24, 25, 24]
-print(len(it_companies))
-it_companies.add("twitter")
-print('twitter' in it_companies)
-com={"com1","compe2","cpm3"}
-it_companies.update(com)
-it_companies.pop()
-print(A.union(B))
-print(B.union(A))
-print(A.isdisjoint(B))
-print(A.symmetric_difference(B))
-del it_companies
-st = set(age)
-if len(age) > len(st):
-    print("The list has duplicate ages")
+dog={}
+dog={"name":"bru","color":"brown","legs":"4"}
+print(dog)
+print(len(dog))
+print(type(dog.values()))
+ls=dog.keys()
+tp=list(ls)
+print(tp)
 
-st=" am a teacher and I love to inspire and teach people."
-stw=[st.split]
-str=set(stw)
-print(st)
-print(str)
