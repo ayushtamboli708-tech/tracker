d811a19b0ec939179ef868034ac8326ec34b1162
Ayush <ayushtamboli708@gmail.com>
2026-06-22 22:41:05 +0530

    practice

--- prac.py
+++ prac.py
@@ -1,10 +1,21 @@
 
-dog={}
-dog={"name":"bru","color":"brown","legs":"4"}
-print(dog)
-print(len(dog))
-print(type(dog.values()))
-ls=dog.keys()
-tp=list(ls)
-print(tp)
+person={
+    'first_name': 'Asabeneh',
+    'last_name': 'Yetayeh',
+    'age': 250,
+    'country': 'Finland',
+    'is_married': True,
+    'skills': ['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+    'address': {
+        'street': 'Space street',
+        'zipcode': '02210'
+    }
+    }
 
+print(person)
+
+if 'skills' in person:
+    mid=person.get('skills')
+    print(mid[(len(mid)-1)//2])
+    if 'Python' in person.get('skills'):
+        print("Python is in the skills list")
