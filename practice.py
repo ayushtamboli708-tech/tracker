51c7765a01de850b440e54ca44d6de60d6e9d649
Ayush <ayushtamboli708@gmail.com>
2026-06-28 23:27:57 +0530

    regex

--- practice.py
+++ practice.py
@@ -1,3 +1,19 @@
-names = ['Finland', 'Sweden', 'Norway','Denmark','Iceland', 'Estonia','Russia']
-*nordic_country, eu,ru = names
-print(nordic_country, eu, ru)
+import re
+text = 'The quick brown fox jumps over the lazy dog'
+match = re.match(r'The quick brown fox', text,re.IGNORECASE)
+print(match)
+print(match.span())
+match = re.search('lazy', text,re.IGNORECASE)
+print(match)
+print(match.span())
+txt = '''Python is the most beautiful language that a human being has ever created.
+I recommend python for a first programming language'''
+matches = re.findall('python', txt,re.IGNORECASE)
+print(matches)
+match = re.sub('python', 'java', txt,re.IGNORECASE)
+print(match)
+txt = '''I am teacher and  I love teaching.
+There is nothing as rewarding as educating and empowering people.
+I found teaching more interesting than any other jobs.
+Does this motivate you to be a teacher?'''
+print(re.split('\t', txt)) 
