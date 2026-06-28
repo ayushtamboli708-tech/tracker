c4d874e7ffd7ddce7e5cb118f1a8e9f61558296f
Ayush <ayushtamboli708@gmail.com>
2026-06-28 23:13:08 +0530

    packingand unpacking

--- practice.py
+++ practice.py
@@ -1,13 +1,3 @@
-from datetime import datetime
-now = datetime.now()
-print(now)                      # 2021-07-08 07:34:46.549883
-day = now.day                   # 8
-month = now.month               # 7
-year = now.year                 # 2021
-hour = now.hour                 # 7
-minute = now.minute             # 38
-second = now.second
-timestamp = now.timestamp()
-print(day, month, year, hour, minute)
-print('timestamp', timestamp)
-print(f'{day}/{month}/{year}, {hour}:{minute}')  # 8/7/2021, 7:38
+names = ['Finland', 'Sweden', 'Norway','Denmark','Iceland', 'Estonia','Russia']
+*nordic_country, eu,ru = names
+print(nordic_country, eu, ru)
