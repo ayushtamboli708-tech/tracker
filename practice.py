661d4f5a8afb85f905670d4da955e90d0a16e592
Ayush <ayushtamboli708@gmail.com>
2026-06-29 00:26:48 +0530

    regex

--- practice.py
+++ practice.py
@@ -1,19 +1,19 @@
 import re
-text = 'The quick brown fox jumps over the lazy dog'
-match = re.match(r'The quick brown fox', text,re.IGNORECASE)
-print(match)
-print(match.span())
-match = re.search('lazy', text,re.IGNORECASE)
-print(match)
-print(match.span())
-txt = '''Python is the most beautiful language that a human being has ever created.
-I recommend python for a first programming language'''
-matches = re.findall('python', txt,re.IGNORECASE)
-print(matches)
-match = re.sub('python', 'java', txt,re.IGNORECASE)
-print(match)
-txt = '''I am teacher and  I love teaching.
-There is nothing as rewarding as educating and empowering people.
-I found teaching more interesting than any other jobs.
-Does this motivate you to be a teacher?'''
-print(re.split('\t', txt)) 
+from collections import Counter
+
+sentence = '''%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!?'''
+
+def clean_text(text):
+    # Remove every character except letters and spaces
+    cleaned = re.sub(r'[^A-Za-z\s]', '', text)
+    return cleaned
+
+def most_frequent_words(text):
+    words = text.split()
+    count = Counter(words)
+    return count.most_common(3)
+
+cleaned_text = clean_text(sentence)
+
+print(cleaned_text)
+print(most_frequent_words(cleaned_text))
