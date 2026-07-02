85c2e4107b2076ab304f1aa013fdb48e51dfee3a
Ayush <ayushtamboli708@gmail.com>
2026-07-03 00:33:04 +0530

    web scrapping

--- File management.py
+++ File management.py
@@ -1,1 +1,17 @@
+import requests
+import json
+from bs4 import BeautifulSoup
+url = 'http://www.bu.edu/president/boston-university-facts-stats/'
+response = requests.get(url)
+content = response.content
+soup = BeautifulSoup(content, 'html.parser')
 
+data = {
+    "title": soup.title.text,
+    "heading": soup.h1.text,
+    "body": soup.body.text
+}
+with open('data.json','w') as  f:
+    json.dump(data,f,indent=4)
+print("Data has been written to data.json")
+print(data)
