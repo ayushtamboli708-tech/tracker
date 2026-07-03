ce3750c3c9ba7526b3f973b7fbbeb5ff604cb0ba
Ayush <ayushtamboli708@gmail.com>
2026-07-04 00:10:37 +0530

    pandas

--- File_management.py
+++ File_management.py
@@ -1,5 +1,16 @@
 import numpy as np
 import pandas as pd
-lst = [1,2,3,4,5]
-arr =np.array(lst)
-print(arr)
+import csv
+try:
+    df = pd.read_csv('data.csv')
+except FileNotFoundError:
+    print("File not found.")
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+titles = df.columns.tolist()
+print(titles)
+lst = df['title'].tolist()
+print(df['title'].head())
+fil = df[df['title'].str.contains("Florida DJs May Face Felony for April Fools' W...", case=False, na=False)]
+print(fil)
